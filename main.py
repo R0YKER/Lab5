759cd8187b50806720e5777396f0724e49b47879
R0YKER <152964692+R0YKER@users.noreply.github.com>
2024-06-16 15:13:47 +0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import array
 import binascii
-
 def task1(dict_list):
     merged_dict = {}
     for dictionary in dict_list:
-        merged_dict.update(dictionary)
+     merged_dict.update(dictionary)
     return merged_dict
-
 def task2(input_string):
     array_name, values_string = input_string.split(':')
     array_type = values_string.split(',')[0].split('(')[1]
@@ -14,7 +12,6 @@ def task2(input_string):
     interpreted_array = array.array(array_type, values)
     bytes_representation = interpreted_array.tobytes()
     return interpreted_array, bytes_representation
-
 def task3(input_array):
     unique_elements = []
     for item in input_array:
@@ -23,47 +20,43 @@ def task3(input_array):
     return unique_elements
 
 def task4(input_array):
-    full_range = set(range(10, 21))
-    input_set = set(input_array)
-    missing_numbers = full_range - input_set
-    
+ full_range = set(range(10, 21))
+ input_set = set(input_array)
+ missing_numbers = full_range - input_set
+
     if missing_numbers:
         return missing_numbers.pop()
     else:
         return None
-    
+
 def task5(dict_list):
     distinct = []
     for dictionary in dict_list:
-        for value in dictionary.values():
-            if value not in distinct:
-                distinct.append(value)
+ for value in dictionary.values():
+if value not in distinct:
+ distinct.append(value)
     unique_count = len(distinct)
     return distinct
-
 def task6(dictionaries):
     letters_lists = [list(dictionary.keys()) for dictionary in dictionaries]
     combinations_count = 1
     for letters_list in letters_lists:
-        combinations_count *= len(letters_list)
+combinations_count *= len(letters_list)
     return combinations_count
-
 def task7(dictionary):
     sorted_keys = sorted(dictionary.keys(), reverse=True)
     largest_keys = sorted_keys[:3]
-    
     return largest_keys
-
 def task8(dict_list):
     combined_values = {}
     for dictionary in dict_list:
-        item = dictionary['item']
-        amount = dictionary['amount']
-        if item in combined_values:
-            combined_values[item] += amount
-        else:
-            combined_values[item] = amount
-    return combined_values
+    item = dictionary['item']
+    amount = dictionary['amount']
+  if item in combined_values:
+  combined_values[item] += amount
+  else:
+combined_values[item] = amount
+   return combined_values
 
 
 
